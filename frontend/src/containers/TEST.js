660ab877c6aa6031df4146a2cbc47d8a1f16d64c
David Koch <davidmuecke@hotmail.de>
2018-04-11 11:20:52 +0200

    Updated GitIgnore

--- frontend/src/containers/TEST.js
+++ frontend/src/containers/TEST.js
@@ -1,11 +1,44 @@
 import React, { Component, Fragment } from "react";
-
+import { API, Auth } from "aws-amplify";
+import config from "../config";
 
 export default class TEST extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            isLoading: false,
+            content: "Hallo"
+        };
+    }
+    handleSubmit = async event => {
+        event.preventDefault();
+
+        if (this.state.content.length == 0) {
+            alert("Please pick a file smaller than 5MB");
+            return;
+        }
+
+        this.setState({ isLoading: true });
+
+        try {
+            var x = await Auth.currentAuthenticatedUser();
+            var reply = await this.createNote({
+                content: this.state.content,
+                user: x
+            });
+            console.log(reply);
+           // this.props.history.push("/");
+        } catch (e) {
+            alert(e);
+            this.setState({ isLoading: false });
+        }
     }
 
+    createNote(note) {
+        return API.post("strawberry", "/arduinoTest", {
+            body: note
+        });
+    }
 
     render() {
         return (
@@ -22,6 +55,7 @@ export default class TEST extends Component {
                         </div>
                     </Fragment>
                 }
+                <button onClick={this.handleSubmit}>Test</button>
             </div>
         );
     }
