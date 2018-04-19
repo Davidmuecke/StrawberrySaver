187d5b596ac604a250e2ec610138461cef094bf7
Pudelbaer <rahel.illi@online.de>
2018-04-19 09:44:35 +0200

    kommentar

--- backend/newSensor.js
+++ backend/newSensor.js
@@ -98,7 +98,7 @@ function editNewSensorForUser(userID,sensorID) {
         }
     }).data.column[2];
     //Wir wollen von dem Item, der zu der entsprechenden userID gehört nur die Werte von "sensors" ausgegeben haben --> 3te Spalte
-    //ich bin mir nicht sicher, ob das tatsächlich so funktioniert??!
+    //ich bin mir nicht sicher, ob das tatsächlich so funktioniert??! --> ansonsten mal mit query versuchen und in der example Datei von Jonathan gucken
 
     //Dabei wird ein JSON zurückgegeben, welcher zu einem Array konvertiert wird, um dann die neue sensorID hinten anzufügen
     // ich bin mir gar nicht sicher, ob hier ein JSON zurück gegeben wird, aber ansonsten ist es noch einfacher!
