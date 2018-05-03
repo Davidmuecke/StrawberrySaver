f1f695c662a9e6d9f2f66dd4490192982fb8f180
David Koch <davidmuecke@hotmail.de>
2018-05-03 14:42:13 +0200

    fixed: Plant Form

--- frontend/src/components/PlantAdd.js
+++ frontend/src/components/PlantAdd.js
@@ -25,7 +25,7 @@ export default class Login extends Component {
                 perfectWater: 500,
                 waterScopeGreen: 200,
                 waterScopeYellow: 150,
-                sensorOptions: "",
+                sensorOptions: [],
                 locationOptions: [{
                     text: 'Stuttgart',
                     value: '2637829',
@@ -51,7 +51,7 @@ export default class Login extends Component {
                 perfectWater: this.props.plants[id][12],
                 waterScopeGreen: this.props.plants[id][13],
                 waterScopeYellow: this.props.plants[id][14],
-                sensorOptions: "",
+                sensorOptions: [],
                 locationOptions: [{
                     text: 'Stuttgart',
                     value: '2637829'
@@ -231,7 +231,12 @@ export default class Login extends Component {
         if (window.innerWidth>="900"){
             seite= "seite1"}
         else{
-            seite="seite2"}
+            seite="seite2"
+        }
+        let defaultOptionSensor="";
+        if(this.state.sensorOptions[0] !== undefined){
+            defaultOptionSensor = this.state.sensorOptions[this.state.sensorOptions.length -1].value;
+        }
         return (
             <div id={seite}>
                 <Container >
@@ -277,12 +282,13 @@ export default class Login extends Component {
                             />
                         </Form.Field>
                         <Form.Field>
-                            <label>Senor</label>
-                            <Dropdown placeholder='Select Sensor' fluid selection options={this.state.sensorOptions} onChange={this.handleChange}/>
+                            <label>Sensor</label>
+                                <Dropdown placeholder='Select Sensor' fluid selection options={this.state.sensorOptions}
+                                          defaultValue={this.state.sensorID}   onChange={this.handleChange}/>
                         </Form.Field>
                         <Form.Field>
                             <label>Standort</label>
-                            <Dropdown placeholder='Standort auswählen' fluid selection options={this.state.locationOptions} onChange={this.handleChange}/>
+                            <Dropdown placeholder='Standort auswählen' fluid selection options={this.state.locationOptions} defaultValue={this.state.locationOptions[0].value} onChange={this.handleChange}/>
                         </Form.Field>
                         <Form.Field>
                             <label>lokaler Standort</label>
