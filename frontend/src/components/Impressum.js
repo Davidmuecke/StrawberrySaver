8b578ca28e7b30787686c768cc1b1d3495471178
Pudelbaer <rahel.illi@online.de>
2018-05-03 15:24:30 +0200

    impressum mittig gesetzt

--- frontend/src/components/Impressum.js
+++ frontend/src/components/Impressum.js
@@ -10,12 +10,14 @@ export default class Impressum extends Component{
             seite="seite2"}
         return (
             <div id={seite}>
-                <div className="impressum">
-                    <h1> Impressum </h1>
-                    <h3 id="impressum_name">Strawberry Saver</h3>
-                    <text>Web Engineering Projekt<br/></text>
-                    <text>TINF16B Enrico Keil 4.Semester<br/></text>
-                    <text>Copyright by David, Niklas, Jonathan und Rahel<br/></text>
+                <div className="align_impressum">
+                    <div className="impressum">
+                        <h1> Impressum </h1>
+                        <h3 id="impressum_name">Strawberry Saver</h3>
+                        <text>Web Engineering Projekt<br/></text>
+                        <text>TINF16B Enrico Keil 4.Semester<br/></text>
+                        <text>Copyright by David, Niklas, Jonathan und Rahel<br/></text>
+                    </div>
                 </div>
             </div>
 
